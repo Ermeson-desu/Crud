59ee6b0a4e8ee25f83198197153cd5dba2283bb6
Ermeson-desu <ermesondesu@gmail.com>
2025-01-07 21:05:49 -0300

    Update Conn.cs

--- Conn.cs
+++ Conn.cs
@@ -7,23 +7,11 @@ namespace SqlServerC
 
     public class Conn
     {
-        private string server = "MySQL80";
-        private string database = "db_melodicca";
-        private string user = "root";
-        private string senha = "1234";
         private const string server = "melodiccaConections";
         private const string database = "db_crud";
         private const string user = "root";
         private const string senha = "1234";
-
-        public static string StrCon
-        {
-            get
-            {
-                return $"Data Source ={server}; Integrated Security= False; Initial Catalog = {database};" +
-                    $"User ID = {user}; Password = {senha};";
-            }
-        }
+        
         //conexão com o banco de dados 
         static public string bancoServidor = $"server = {server}; database = {database}; user id = {user}; password = {senha}";
         
