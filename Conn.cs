15aefb2cbb5596c71eac783be4109622f02b38a8
Ermeson-desu <ermesondesu@gmail.com>
2025-01-07 21:01:44 -0300

    update

--- Conn.cs
+++ Conn.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Windows;
+using System.Data.SqlClient;
 
 namespace SqlServerC
 {
@@ -9,6 +11,10 @@ namespace SqlServerC
         private string database = "db_melodicca";
         private string user = "root";
         private string senha = "1234";
+        private const string server = "melodiccaConections";
+        private const string database = "db_crud";
+        private const string user = "root";
+        private const string senha = "1234";
 
         public static string StrCon
         {
@@ -18,5 +24,8 @@ namespace SqlServerC
                     $"User ID = {user}; Password = {senha};";
             }
         }
+        //conexão com o banco de dados 
+        static public string bancoServidor = $"server = {server}; database = {database}; user id = {user}; password = {senha}";
+        
     }
 }
